d987612b8bd3346b5473a4c91fbc86c4df6344ce
MauroPalgiLS <108412978+MauroPalgiLS@users.noreply.github.com>
2024-08-19 11:40:49 -0300

    fix prefab position

--- Assets/Scripts/Managers/RoadManager.cs
+++ Assets/Scripts/Managers/RoadManager.cs
@@ -53,32 +53,61 @@ public class RoadManager : Singleton<RoadManager>
         for (int i = 0; i < _roads.Count; i++)
         {
             ScriptableRoad road = _roads[i];
+            ScriptableRoad nextRoad = null;
+            if (i + 1 < _roads.Count)
+            {
+                nextRoad = _roads[i + 1];
+                Debug.Log(nextRoad.RoadType);
+
+            }
+            Debug.Log(road.RoadType);
+
             Instantiate(road.Prefab, spawnPosition, spawnOrientation);
 
             switch (i)
             {
                 case 0:
+                    spawnPosition += globalOrientation * 20;
+                    break;
                 case 1:
-                    globalOrientation = Vector3.forward;
-                    spawnOrientation = Quaternion.identity;
+                    spawnPosition += globalOrientation * 30;
+                    // globalOrientation = Vector3.forward;
+                    // spawnOrientation = Quaternion.identity;
                     break;
                 case 2:
-                case 3:
-                case 4:
                     globalOrientation = Vector3.left;
                     spawnOrientation = Quaternion.Euler(0, -90, 0);
+                    spawnPosition += globalOrientation * 30;
+                    break;
+                case 3:
+                    spawnPosition += globalOrientation * 20;
+                    break;
+                case 4:
+                    spawnPosition += globalOrientation * 30;
                     break;
                 case 5:
-                case 6:
-                case 7:
                     globalOrientation = Vector3.back;
                     spawnOrientation = Quaternion.Euler(0, 180, 0);
+                    spawnPosition += globalOrientation * 30;
+                    break;
+                case 6:
+                    spawnPosition += globalOrientation * 20;
+                    break;
+                case 7:
+
+                    spawnPosition += globalOrientation * 30;
                     break;
                 case 8:
-                case 9:
-                case 10:
                     globalOrientation = Vector3.right;
                     spawnOrientation = Quaternion.Euler(0, 90, 0); ;
+                    spawnPosition += globalOrientation * 30;
+                    break;
+                case 9:
+                    spawnPosition += globalOrientation * 20;
+                    break;
+                case 10:
+
+                    spawnPosition += globalOrientation * 30;
                     break;
 
                 default:
@@ -86,8 +115,10 @@ public class RoadManager : Singleton<RoadManager>
             }
             Debug.Log(globalOrientation);
             Debug.Log("spawn position " + spawnPosition);
+            Debug.Log("spawn Lenght " + road.BaseStats.prefabLenght);
+            // spawnPosition += globalOrientation * (road.RoadType.Equals(RoadType.Straight) ? 20 : 30);
 
-            spawnPosition += (globalOrientation * road.BaseStats.prefabLenght);
+
         }
     }
 }
